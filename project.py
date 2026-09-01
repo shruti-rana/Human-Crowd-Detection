5d8786b274ab84f71fe5469d7b54600fb6d43493
shruti-rana <shrutirana179@gmail.com>
2026-09-01 13:27:22 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,22 +1,3 @@
-### Brief Description
-
-
-# 1. OpenCV (`cv2`) is used to read and display the video.
-# 2. YOLOv8 is loaded using the `yolov8s.pt` pretrained model.
-# 3. The video `croud.mp4` is read frame by frame.
-# 4. YOLO detects only people (class 0) in each frame.
-# 5. A green bounding box and `"Person"` label are drawn around each detected person.
-# 6. The program counts the detected people in the current frame.
-# 7. The People Count is displayed in red at the top of the video.
-# 8. Pressing ESC stops the program.
-# 9. Finally, the video and OpenCV windows are released and closed.
-
-# In short: It performs real-time person detection and frame-by-frame people counting** from a video using YOLOv8.
-
-
-
-
-
 import cv2
 from ultralytics import YOLO
 
